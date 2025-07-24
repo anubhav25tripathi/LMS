19c1977b30179db2c6b84017bbc22161c054c08a
anubhav25tripathi <anubhav25tripathi2003@gmail.com>
2025-07-25 03:02:54 +0530

    some-bugs after deployment of full project

--- server/server.js
+++ server/server.js
@@ -15,7 +15,13 @@ await connectDB();
 await connectCloudinary();
 
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://courseseller-frontend.vercel.app',
+  credentials: true,
+}));
+
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(clerkMiddleware())
 // routes
 app.get('/', (req, res) => {
